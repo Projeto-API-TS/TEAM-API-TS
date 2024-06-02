551b38d3b1ef8e75401e23918e27ca3794b0b2fc
RenatoFB8 <tenat.renato@gmail.com>
2024-06-02 17:50:52 -0300

    feat: add patch /teams/:team_id route and update related functions to teams

--- src/controllers/teamsController.ts
+++ src/controllers/teamsController.ts
@@ -3,77 +3,20 @@ import ISquad from "../interfaces/squad";
 import teamsServices from "../services/teamsServices";
 import { Request, Response } from "express";
 
+const createTeam = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { name, leaderId } = req.body;
 
+        const newTeam: ISquad = await teamsServices.createTeam(name, leaderId);
 
-const createTeam = async(req:Request, res:Response):Promise<void>=>{
-    try{
-        const { name, leaderId }=req.body;
-
-        const newTeam:ISquad = await teamsServices.createTeam(name, leaderId);
-
-        const response ={
-            data:newTeam,
-            error:null,
-            status:200,
-        }
+        const response = {
+            data: newTeam,
+            error: null,
+            status: 200,
+        };
 
         res.status(200).json(response);
-
-
-    }catch(e:any){
-        console.error(e);
-        res.status(e.status || 500).json({
-            data: null,
-            error: e.message,
-            status: e.status || 500
-        });
-    }
-}
-
-const getAllTeams=async(req:Request,res:Response):Promise<void>=>{
-    try{
-        const teams:ISquad[]=await teamsServices.getAllTeams();
-
-        const response ={
-            data:teams,
-            error:null,
-            status:200,
-        }
-
-        res.status(200).json(response);
-
-    }catch(e:any){
-        console.error(e);
-        res.status(e.status || 500).json({
-            data: null,
-            error: e.message,
-            status: e.status || 500
-        });
-    }
-}
-
-const getTeamById = async (req:Request,res:Response):Promise<void>=>{
-    try{
-        const { team_id } = req.params;
-        const team:ISquad|null = await teamsServices.getTeamById(team_id);
-
-        if(team){
-            const response ={
-                data:team,
-                error:null,
-                status:200,
-            }
-    
-            res.status(200).json(response);
-
-        }else{
-            res.status(404).json({
-                data: null,
-                error: 'Equipe não encontrada.',
-                status: 404,
-            });
-        }
-    }catch (e: any) {
+    } catch (e: any) {
         console.error(e);
         res.status(e.status || 500).json({
             data: null,
@@ -81,11 +24,81 @@ const getTeamById = async (req:Request,res:Response):Promise<void>=>{
             status: e.status || 500,
         });
     }
-}
+};
 
+const getAllTeams = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const teams: ISquad[] = await teamsServices.getAllTeams();
+
+        const response = {
+            data: teams,
+            error: null,
+            status: 200,
+        };
+
+        res.status(200).json(response);
+    } catch (e: any) {
+        console.error(e);
+        res.status(e.status || 500).json({
+            data: null,
+            error: e.message,
+            status: e.status || 500,
+        });
+    }
+};
+
+const getTeamById = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { team_id } = req.params;
+        const team: ISquad | null = await teamsServices.getTeamById(team_id);
+
+        if (team) {
+            const response = {
+                data: team,
+                error: null,
+                status: 200,
+            };
+
+            res.status(200).json(response);
+        } else {
+            res.status(404).json({
+                data: null,
+                error: "Equipe não encontrada.",
+                status: 404,
+            });
+        }
+    } catch (e: any) {
+        console.error(e);
+        res.status(e.status || 500).json({
+            data: null,
+            error: e.message,
+            status: e.status || 500,
+        });
+    }
+};
+
+const updateTeam = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { name, leaderId } = req.body;
+        const id = req.params.id;
+
+        const updatedTeam = await teamsServices.updateTeam(id, name, leaderId);
+
+        const response: IAPIResponse<Partial<ISquad>> = {
+            data: updatedTeam,
+            error: null,
+            status: 200,
+        };
+        res.status(200).json(response);
+    } catch (e: any) {
+        console.error(e);
+        res.status(e.status || 500).json({ data: null, error: e.message });
+    }
+};
 
 export default {
     createTeam,
     getAllTeams,
     getTeamById,
+    updateTeam
 };
--- src/services/teamsServices.ts
+++ src/services/teamsServices.ts
@@ -1,25 +1,63 @@
 import teamsRepository from "../repository/teamsRepository";
 import ISquad from "../interfaces/squad";
+import { validateTeamName, validateUUID } from "../utils/validator";
+import CustomError from "../utils/customError";
 
-const createTeam = async (name:string,leaderId:string):Promise<ISquad>=>{
-    const newTeam:ISquad = await teamsRepository.createTeam(name,leaderId);
+const createTeam = async (name: string, leaderId: string): Promise<ISquad> => {
+    const newTeam: ISquad = await teamsRepository.createTeam(name, leaderId);
     return newTeam;
-}
+};
 
-const getAllTeams = async ():Promise<ISquad[]>=>{
-    const teams:ISquad[] = await teamsRepository.getAllTeams();
+const getAllTeams = async (): Promise<ISquad[]> => {
+    const teams: ISquad[] = await teamsRepository.getAllTeams();
     return teams;
-}
+};
 
-const getTeamById = async (team_id:string):Promise<ISquad | null>=>{
-    const team:ISquad|null = await teamsRepository.getTeamById(team_id);
+const getTeamById = async (team_id: string): Promise<ISquad | null> => {
+    const team: ISquad | null = await teamsRepository.getTeamById(team_id);
     return team;
-}
+};
 
+const updateTeam = async (id: string, name: string, leaderId: string) : Promise<ISquad> => {
+    try {
+        if (!id) {
+            throw new CustomError("O id é obrigatotrio.", 400);
+        }
 
+        if (!validateUUID(id)) {
+            throw new CustomError("ID de time invalido.", 400);
+        }
 
+        if (name && !validateTeamName(name)) {
+            throw new CustomError("O nome do time deve ter entre 3 e 30 caracteres e conter apenas letras e espaços.", 400);
+        }
+        
+        if (leaderId && !validateUUID(leaderId)) {
+            throw new CustomError("ID de lider invalido.", 400);
+        }
+
+        const isAlreadyLeader = await teamsRepository.getTeamByLeader(leaderId);
+
+        const oldTeam: ISquad | null = await teamsRepository.getTeamById(id);
+        
+        if (!oldTeam) {
+            throw new CustomError("Equipe não encontrada.", 404);
+        }
+
+        if (isAlreadyLeader && isAlreadyLeader.leader !== oldTeam.leader) {
+            throw new CustomError("O lider fornecido já está liderando uma equipe.", 400);
+        }
+
+        const updatedTeam = await teamsRepository.updateTeam(id, name || oldTeam.name , leaderId || oldTeam.leader)
+
+        return updatedTeam;
+    } catch (e) {
+        throw e;
+    }
+};
 export default {
     createTeam,
     getAllTeams,
     getTeamById,
+    updateTeam,
 };
