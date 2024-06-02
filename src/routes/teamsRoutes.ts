5fad863f7a24f8b3925ad33f54e824ca0fcf1601
Gabriela <gabisgamba@gmail.com>
2024-06-02 05:41:27 -0300

    feat: add teams routes for createTeam, getAllteams, getTeamById

--- src/routes/teamsRoutes.ts
+++ src/routes/teamsRoutes.ts
@@ -1,4 +1,12 @@
 import express from "express";
 const router = express.Router();
+import teamController from '../controllers/teamsController'
+
+
+router.post('/', teamController.createTeam);
+
+router.get('/', teamController.getAllTeams);
+
+router.get('/:team_id', teamController.getTeamById);
 
 export default router;
