20a3736ad7dbfb9e09076595e4cb11415c07d906
Gabriela <gabisgamba@gmail.com>
2024-06-02 08:09:32 -0300

    feat: create the Repository to createTeam, getAllTeams, getTeamById

--- src/repository/teamsRepository.ts
+++ src/repository/teamsRepository.ts
@@ -1,5 +1,58 @@
 import pool from "../database/postgresql";
 import ISquad from "../interfaces/squad";
+import CustomError from "../utils/customError";
+
+const getTeamById = async (team_id: string): Promise<ISquad | null> => {
+    const query = "SELECT * FROM teams WHERE id = $1";
+    let client;
+    try {
+        client = await pool.connect();
+        const { rows } = await client.query(query, [team_id]);
+        return rows[0] || null; 
+    } catch (e: any) {
+        throw new CustomError(e.message, 500);
+    } finally {
+        if (client) {
+            client.release();
+        }
+    }
+};
+
+const getAllTeams = async (): Promise<ISquad[]> => {
+    const query = "SELECT * FROM teams";
+    let client;
+    try {
+        client = await pool.connect();
+        const { rows } = await client.query(query);
+        return rows; 
+    } catch (e: any) {
+        throw new CustomError(e.message, 500);
+    } finally {
+        if (client) {
+            client.release();
+        }
+    }
+};
+
+const createTeam = async (name: string, leaderId: string): Promise<ISquad> => {
+    const query = `INSERT INTO teams (name, leader) VALUES ($1, $2) RETURNING *`;
+    let client;
+    try {
+        client = await pool.connect();
+        const { rows } = await client.query(query, [name, leaderId]);
+        return rows[0];
+    } catch (e: any) {
+        throw new CustomError(e.message, 500);
+    } finally {
+        if (client) {
+            client.release();
+        }
+    }
+};
+
 
 export default {
+    getTeamById,
+    getAllTeams,
+    createTeam,
 };
