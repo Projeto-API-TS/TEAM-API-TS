1de847211521ca45750d0f8191fda9cecb93f2e7
RenatoFB8 <tenat.renato@gmail.com>
2024-06-03 16:17:15 -0300

    fix: fixed teamRoutes.ts

--- src/routes/teamsRoutes.ts
+++ src/routes/teamsRoutes.ts
@@ -3,22 +3,13 @@ const router = express.Router();
 import teamController from "../controllers/teamsController";
 import authMiddleware from "../middleware/auth";
 
-<<<<<<< users
-router.post("/teams", teamController.createTeam);
-router.get("/teams", teamController.getAllTeams);
-router.get("/teams/:team_id/members", authMiddleware, teamController.getTeamMembers);
-router.get("/teams/:team_id", teamController.getTeamById);
-router.patch("/teams/:id", teamController.updateTeam);
-router.delete("/teams/:team_id", teamController.updateTeam);
-router.delete("/teams/:team_id/member/:user_id",authMiddleware, teamController.deleteTeamMember)
-=======
 router.get("/", teamController.getAllTeams);
 router.get("/:team_id/members", authMiddleware, teamController.getTeamMembers);
 router.get("/:team_id", teamController.getTeamById);
 router.post("/:team_id/member/:user_id", authMiddleware, teamController.insertMember)
 router.post("/", teamController.createTeam);
 router.patch("/:id", teamController.updateTeam);
-router.delete("/:team_id", teamController.updateTeam);
->>>>>>> main
+router.delete("/:team_id", teamController.deleteTeamById);
+router.delete("/teams/:team_id/member/:user_id",authMiddleware, teamController.deleteTeamMember);
 
 export default router;
